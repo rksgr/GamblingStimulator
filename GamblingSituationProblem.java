ef0f6c77e2b7b2e3601774bc94e2a5012b70bcb4
rksgr <r_kjha@yahoo.com>
2021-12-12 18:40:56 +0530

    Initialize variables

---
+++ GamblingSituationProblem.java
@@ -0,0 +1,13 @@
+package com.mycompany.workshop_dec_11;
+
+
+public class GamblingSituationProblem {
+    /*
+    Use case 1
+    */
+    public static final int INITIAL_BET_STAKE = 100;
+    public static final int BET_AMT = 1;
+    public static void main(String []args){
+        
+    }
+}
