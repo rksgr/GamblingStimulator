5f2ad8099a2d67a3ed5ae97714060ecf8453efe4
rksgr <r_kjha@yahoo.com>
2021-12-12 19:54:48 +0530

    Calculative Gambler Case

--- GamblingSituationProblem.java
+++ GamblingSituationProblem.java
@@ -24,8 +24,28 @@ public class GamblingSituationProblem {
             +remg_amt+ " with him after one bet.");
         } 
     }
+    /*
+    Use case 3
+    */
+    public void gamblingSituationProblemUC3(int margin_perct){
+        double uppr_margin = INITIAL_BET_STAKE + margin_perct*INITIAL_BET_STAKE/100;
+        double lowr_margin = INITIAL_BET_STAKE - margin_perct*INITIAL_BET_STAKE/100;
+        double avail_amt = INITIAL_BET_STAKE;
+        while ((avail_amt<uppr_margin) && (avail_amt>lowr_margin)){
+            double random_num = Math.random();
+            if (random_num>0.5){
+                avail_amt += BET_AMT;
+            }else if (random_num<=0.5){
+                avail_amt -= BET_AMT;
+            }
+        }
+        System.out.println("The gambler resigns for the day after he has $"
+        + avail_amt + " with him.");
+    }
     public static void main(String []args){
         GamblingSituationProblem gsp2 = new GamblingSituationProblem();
-        gsp2.gamblingSituationProblemUC2();        
+        //gsp2.gamblingSituationProblemUC2(); 
+        GamblingSituationProblem gsp3 = new GamblingSituationProblem();
+        gsp3.gamblingSituationProblemUC3(50);
     }
 }
