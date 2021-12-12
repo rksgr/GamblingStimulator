798bb3082c2bb7d1e226171a5289392cb6be83f6
rksgr <r_kjha@yahoo.com>
2021-12-12 19:24:55 +0530

    Gambler bets

--- GamblingSituationProblem.java
+++ GamblingSituationProblem.java
@@ -7,7 +7,25 @@ public class GamblingSituationProblem {
     */
     public static final int INITIAL_BET_STAKE = 100;
     public static final int BET_AMT = 1;
+    
+    /*
+    Use Case 2
+    */
+    public void gamblingSituationProblemUC2(){
+        double random_num = Math.random();
+        int remg_amt = INITIAL_BET_STAKE;
+        if (random_num>0.5){
+            remg_amt += BET_AMT;
+            System.out.println("The Gambler wins the bet and he has $"
+            +remg_amt+ " with him after one bet.");
+        }else if (random_num<=0.5){
+            remg_amt -= BET_AMT;
+            System.out.println("The Gambler loses the bet and he has $"
+            +remg_amt+ " with him after one bet.");
+        } 
+    }
     public static void main(String []args){
-        
+        GamblingSituationProblem gsp2 = new GamblingSituationProblem();
+        gsp2.gamblingSituationProblemUC2();        
     }
 }
